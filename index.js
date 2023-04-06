665f8c882854fbfed5ace0bb1cca985577a90ee1
xeift <xeift.c@gmail.com>
2023-04-06 18:30:15 +0800

    minor fixes

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import init from './pkg/web_application.js';
 init();
 import * as wasm from './pkg/web_application.js';
 
-const helloBtn = document.getElementById("helloBtn");
-helloBtn.addEventListener("click", function() {
+const helloBtn = document.getElementById('helloBtn');
+helloBtn.addEventListener('click', function() {
     console.log(wasm.greet())}
 );
 
-const sendMessageBtn = document.getElementById("sendMessageBtn");
-sendMessageBtn.addEventListener("click", function() {
+const sendMessageBtn = document.getElementById('sendMessageBtn');
+sendMessageBtn.addEventListener('click', function() {
     const PRIVATEKEY = document.getElementById('privatekeyInput').value;
     console.log(PRIVATEKEY);
     console.log(wasm.send_message(PRIVATEKEY))}
