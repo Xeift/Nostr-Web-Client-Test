f339871eb9d8e330706cde0fee0810e2c3d34a34
xeift <xeift.c@gmail.com>
2023-04-06 11:29:25 +0800

    pass privatekey to function

--- src/lib.rs
+++ src/lib.rs
@@ -18,20 +18,25 @@ pub fn greet() -> i32 {
     return 35235;
 }
 
+
+async fn lol2(input: String) {
+    // const BECH32_SK: &str = "nsec1lyc8k4krke9s72zatj8nckx99w6348kd7zc9n7k8dszvg86dp48s3gp8fy";
+    let BECH32_SK: &str = &input[..];
+    let secret_key = SecretKey::from_bech32(BECH32_SK).unwrap();
+    let keys = Keys::new(secret_key);
+    let client = Client::new(&keys);
+    client.add_relay("wss://relay.snort.social").await.unwrap();
+    client.connect().await;
+
+    let event_id = client
+        .publish_text_note("Testing nostr-sdk WASM", &[])
+        .await
+        .unwrap();
+}
+
+
 #[wasm_bindgen]
-pub fn lol() {
-    spawn_local(async {
-        const BECH32_SK: &str = "nsec1lyc8k4krke9s72zatj8nckx99w6348kd7zc9n7k8dszvg86dp48s3gp8fy";
-        let secret_key = SecretKey::from_bech32(BECH32_SK).unwrap();
-        let keys = Keys::new(secret_key);
-        let client = Client::new(&keys);
-    
-        client.add_relay("wss://relay.snort.social").await.unwrap();
-        client.connect().await;
-    
-        let event_id = client
-            .publish_text_note("Testing nostr-sdk WASM", &[])
-            .await
-            .unwrap();
-    })
+pub fn lol(input: &str) {
+    let input = input.clone().to_string();
+    spawn_local(lol2(input));
 }
