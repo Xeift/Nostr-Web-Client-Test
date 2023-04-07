e2f5e44f58a64ca4ca75556a47a3722cc7683c6c
xeift <xeift.c@gmail.com>
2023-04-07 15:13:49 +0800

    implement store privatekey function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import init from './pkg/web_application.js';
 init();
 import * as wasm from './pkg/web_application.js';
 
+
 const helloBtn = document.getElementById('helloBtn');
 helloBtn.addEventListener('click', function() {
     console.log(wasm.greet())
@@ -10,6 +11,7 @@ helloBtn.addEventListener('click', function() {
     console.log(myString)
 });
 
+
 const sendMessageBtn = document.getElementById('sendMessageBtn');
 sendMessageBtn.addEventListener('click', function() {
     const PRIVATEKEY = document.getElementById('privatekeyInput').value;
@@ -17,3 +19,11 @@ sendMessageBtn.addEventListener('click', function() {
     console.log(wasm.send_message(PRIVATEKEY))}
     // nsec1tacg5mhajrp3usad50rne7r4st8j4chsdydjmkjhmeh83erc34hsxxfffc
 );
+
+
+const storePrivatekeyBtn = document.getElementById('storePrivatekeyBtn');
+storePrivatekeyBtn.addEventListener('click', function() {
+    const PRIVATEKEY = document.getElementById('privatekeyInput').value;
+    localStorage.setItem('NOSTR_PRIVATE_KEY', PRIVATEKEY)
+    console.log(localStorage.getItem('NOSTR_PRIVATE_KEY'))
+})
