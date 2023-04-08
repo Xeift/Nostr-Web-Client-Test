38dd9a89490f46e5cf964e680326dd6af24c602b
xeift <xeift.c@gmail.com>
2023-04-08 10:23:39 +0800

    read privatekey from local storge

--- website/index.js
+++ website/index.js
@@ -6,24 +6,21 @@ import * as wasm from '../pkg/web_application.js';
 const helloBtn = document.getElementById('helloBtn');
 helloBtn.addEventListener('click', function() {
     console.log(wasm.greet())
-    localStorage.setItem('myString', 'asdfg12345');
-    let myString = localStorage.getItem('myString');
-    console.log(myString)
 });
 
 
 const sendMessageBtn = document.getElementById('sendMessageBtn');
 sendMessageBtn.addEventListener('click', function() {
-    const PRIVATEKEY = document.getElementById('privatekeyInput').value;
+    const PRIVATEKEY = localStorage.getItem('NOSTR_PRIVATE_KEY');
+    console.log('------------------------');
     console.log(PRIVATEKEY);
-    console.log(wasm.send_message(PRIVATEKEY))}
-    // nsec1tacg5mhajrp3usad50rne7r4st8j4chsdydjmkjhmeh83erc34hsxxfffc
-);
+    console.log(wasm.send_message(PRIVATEKEY))
+    console.log('------------------------\n');
+});
 
-
+// nsec1tacg5mhajrp3usad50rne7r4st8j4chsdydjmkjhmeh83erc34hsxxfffc
 const storePrivatekeyBtn = document.getElementById('storePrivatekeyBtn');
 storePrivatekeyBtn.addEventListener('click', function() {
     const PRIVATEKEY = document.getElementById('privatekeyInput').value;
     localStorage.setItem('NOSTR_PRIVATE_KEY', PRIVATEKEY)
-    console.log(localStorage.getItem('NOSTR_PRIVATE_KEY'))
 })
